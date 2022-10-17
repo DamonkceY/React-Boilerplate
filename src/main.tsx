6c16e9517ec1e76cb9045c4324b792b33c4f8177
DamonkceY <medchouiref@outlook.com>
2022-10-17 15:58:11 +0100

    husky hooks test

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import {Provider} from 'react-redux';
 import './core/i18n';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+
 	<Provider store={store}>
 		<EntryPoint/>
 	</Provider>
